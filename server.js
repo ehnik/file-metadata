19e5d77718dd221e731648d283d42bab6bd0d08f
Kay Ehni <kay.ehnigmail.com>
2018-01-26 21:08:35 -0800

    added simply error handling if no file submitted

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.get('/', (req,res)=>res.send("./public/index.html"))
 //parses uploaded file and returns file size
 app.post('/', upload.single('file'), (req,res,next)=> {
   res.setHeader('Content-Type', 'application/json');
-  res.send( JSON.stringify({size: req.file.size}) )
+  let response = req.file ? {size: req.file.size} : "No file submitted"
+  res.send( JSON.stringify(response) )
 })
 
 app.listen(3000)
